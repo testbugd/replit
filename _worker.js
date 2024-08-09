1dd2045074e5ff12af44fad4b53f9ebf80ca0301
testbugd <testbug.hk.1@gmail.com>
2024-08-09 10:30:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="218a0b63-2a70-4d6e-8c0d-b0ef07f7196c-00-1v0q6ydk4pqo6.picard.replit.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
